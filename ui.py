ab37e8fa4e29048513ceffa2f29bec730355c55c
mgarzia3 <marco.a.garzia@gmail.com>
2026-03-24 20:13:33 +0100

    Add weight input

--- ui.py
+++ ui.py
@@ -3,19 +3,21 @@ from models import Grade
 def input_grade() -> Grade | None:
     while True:
         try:
-            raw = input("Note eingeben oder 'q' für Quit: ").strip().lower()
+            raw_value = input("Note eingeben oder 'q' für Quit: ").strip().lower()
+            if raw_value == "q":
+                return None            
+            value = float(raw_value)
 
-            if raw == "q":
-                return None
-            
-            value = float(raw)
-            grade = Grade(value)
+            raw_weight = input("Bitte Gewichtung der Note eingeben: ")
+            weight = float(raw_weight)
+
+            grade = Grade(value, weight)
 
             if grade.is_valid():
                 return grade
             
             print("Note muss zwischen 1 und 6 liegen.")
-            
+
         except ValueError:
             print("Ungültige Eingabe. Bitte eine Zahl oder 'q' eingeben.")
         except EOFError:
