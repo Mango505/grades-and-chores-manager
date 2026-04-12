4db653271440d5fd8bccc3aa205addbf57fc5901
mgarzia3 <marco.a.garzia@gmail.com>
2026-04-12 18:37:35 +0200

    Update version number to v1.4.0

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ui import print_menu, print_title, add_grade, redeem, show_overview, filter
 from storage import save_app_config, load_app_config, save_subjects, load_subjects, save_wallet, load_wallet, save_reward_config, load_reward_config, APPCONFIG_PATH, DATA_PATH, WALLET_PATH, REWARDCONFIG_PATH
 from models import RewardConfig
 
-VERSION = "v1.3.1"
+VERSION = "v1.4.0"
 
 def main():
     # --- Argument parser ---
